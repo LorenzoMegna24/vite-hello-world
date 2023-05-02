d075b92baaadbaa16fb2beeb8f5ce6711b70197b
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-02 18:04:53 +0200

    Esercizio completato

---
+++ src/components/PrimoComponente.vue
@@ -0,0 +1,23 @@
+<script>
+  export default{
+    name: "PrimoComponente",
+    data(){
+      return{
+       
+      }
+    },
+    methods: {
+
+    },
+  }
+</script>
+
+<template>
+  <div>
+    <h1>La mia prima app con Vite!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
